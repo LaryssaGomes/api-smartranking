a25f7e60c031fe0d41af7168a5d6fb93d3c59646
Laryssa <>
2021-11-25 23:22:11 -0300

    aula 20

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:dRjJTsyKLreB3u9@cluster0.c9j9s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    JogadoresModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
